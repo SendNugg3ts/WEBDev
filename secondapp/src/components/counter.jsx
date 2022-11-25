0a6818883fe671903e2c0b08cecc30f3d209217b
SendNugg3ts <luis.fmsilva@hotmail.com>
2022-11-25 17:59:08 +0000

    new updates

--- secondapp/src/components/counter.jsx
+++ secondapp/src/components/counter.jsx
@@ -3,25 +3,35 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    ImageUrl: "https://picsum.photos/200",
+    tags: ["tag1", "tag2", "tag3"],
+  };
+  rendertags() {
+    if (this.state.tags.length === 0) return <p>There are no tags to show</p>;
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+  muda = () => {
+    if (this.state.count === 0) {
+      return <p>Zero</p>;
+    }
+  };
+  clicamento = () => {
+    this.setState({ count: this.state.count + 1 });
   };
   render() {
     return (
       <React.Fragment>
-        <img src={this.state.ImageUrl} alt="" />
-        <span className="badge bade-primary">{this.formatCount()}</span>
-        <button>coisa</button>
+        <button onClick={this.clicamento} className="btn btn-secondary btn-sm">
+          Coisa
+        </button>
+        <span className="badge badge-primary">{this.muda}</span>
       </React.Fragment>
     );
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    if (count === 0) {
-      return <h1>Zero</h1>;
-    } else {
-      return count;
-    }
   }
 }
 export default Counter;
--- secondapp/src/index.js
+++ secondapp/src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
+import Counters from "./components/counters";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Counter />,
+  <Counters />,
   <React.StrictMode>
     <App />
   </React.StrictMode>
