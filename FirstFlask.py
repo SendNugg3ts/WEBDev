f5a8bc02d6da7b11711be594eff06e74e110c0ee
SendNugg3ts <luis.fmsilva@hotmail.com>
2023-03-08 15:19:09 +0000

    Flask first site

---
+++ FirstFlask.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import json
+import requests
+
+app=Flask(__name__,template_folder="/home/sendnuggets/Desktop/Learning-Web-Dev")
+
+def get_meme():
+    url="https://meme-api.com/gimme"
+    response=json.loads(requests.request("GET",url).text)
+    Bmeme= response["preview"][-2]
+    subreddit=response["subreddit"]
+    return Bmeme, subreddit
+
+
+@app.route("/")
+
+def test():
+    meme_chosen, subreddit = get_meme()
+    return render_template("meme.html",meme_chosen=meme_chosen,subreddit=subreddit)
+
+app.run(host="0.0.0.0",port=5000)
